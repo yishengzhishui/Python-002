0b232c039aac83aae87a91f9545388a3a542794d
x_wang <18912540921@163.com>
2020-08-02 22:26:01 +0800

    rewrite

--- week01/spiders/spiders/spiders/movies.py
+++ week01/spiders/spiders/spiders/movies.py
@@ -12,21 +12,10 @@ class MoviesSpider(scrapy.Spider):
         yield scrapy.Request(url=self.start_urls[0], callback=self.parse)
 
     def parse(self, response):
-        movies = Selector(response=response).xpath('//div[@class="channel-detail movie-item-title"]')
+        movies = Selector(response=response).xpath('//div[@class="movie-hover-info"]')
         for movie in movies[:10]:
             item = SpidersItem()
-            title = movie.xpath('./a/text()')
-            link = movie.xpath('./a/@href')
-            item['title'] = title.extract_first().strip()
-            item['link'] = 'https://maoyan.com' + link.extract_first().strip()  # 拼接完整的 url
-            yield scrapy.Request(url=item['link'], meta={'item': item}, callback=self.parse2)
-
-    def parse2(self, response):
-        item = response.meta['item']
-        info = Selector(response=response).xpath('//div[@class="movie-brief-container"]')
-        types = info.xpath('./ul/li/a/text()').extract()
-        date = info.xpath('./ul/li[last()]/text()').extract_first().strip()
-        item['types'] = types
-        item['date'] = date
-
-        yield item
+            item['title'] = movie.xpath('./div[1]/span[1]/text()').extract_first()
+            item['types'] = movie.xpath('./div[2]/text()').extract()[1].strip()
+            item['date'] = movie.xpath('./div[4]/text()').extract()[1].strip()
+            yield item
